75032fac4bcc557afe087ab8494453132ed793d4
Sadique Ali <sadiqalikm@gmail.com>
2011-11-01 17:32:18 +0530

    Pulled out variables in inject.rb

--- ruby/Day2/inject.rb
+++ ruby/Day2/inject.rb
@@ -1,5 +1,9 @@
 arr = (1..10).to_a
 puts "array= #{arr.inspect}"
 puts "sum= #{ arr.inject :+ }"
-puts "56 * product=  #{ arr.reduce :* }"
-puts "largest= #{ arr.shuffle.inject {|largest, e| e > largest ? e : largest} }"
+
+product = arr.reduce 56, :*
+puts "56 * product=  #{product}"
+
+largest = arr.shuffle.inject {|largest, e| e > largest ? e : largest}
+puts "largest= #{largest}"
